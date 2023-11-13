aed8ca3a33dcd39d214da378b98cf66842a61fde
oakley <oakleycord@proton.me>
2023-11-12 20:42:47 -0500

    use python3 in runtime

--- nix/package.nix
+++ nix/package.nix
@@ -3,7 +3,7 @@
 stdenv.mkDerivation rec {
   name = "pp-to-amd-epp";
 
-  propagatedBuildInputs = [ pkgs.power-profiles-daemon ];
+  propagatedBuildInputs = [ pkgs.power-profiles-daemon pkgs.python3 ];
   unpackPhase = ":";
   installPhase = "install -m755 -D ${../pp-to-amd-epp} $out/bin/pp-to-amd-epp";
 
