37e68b17bf30d8cd2e21195fc35783867af016b7
oakley <oakleycord@proton.me>
2023-11-12 20:13:19 -0500

    add meta to package.nix

--- nix/package.nix
+++ nix/package.nix
@@ -4,6 +4,13 @@ stdenv.mkDerivation rec {
   name = "pp-to-amd-epp";
   unpackPhase = ":";
   installPhase = "install -m755 -D ${../pp-to-amd-epp} $out/bin/pp-to-amd-epp";
+
+
+  meta = {
+    description = "Power Profiles Daemon to AMD P-STATE EPP";
+    mainProgram = "pp-to-amd-epp";
+  };
+
 }
 
 
